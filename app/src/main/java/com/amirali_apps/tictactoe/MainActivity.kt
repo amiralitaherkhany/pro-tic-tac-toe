9ca4ac07646e9b1d347a890f4e94dff8e3059ca4
amiralitaherkhany <amiralitaherkhany07@gmail.com>
2025-04-28 00:31:34 +0330

    Add a rate dialog
    This commit introduces a new rate dialog to the app.
    - Added new string resources for the rate dialog in both English and Farsi.
    - Implemented the `RateDialog` composable function.
    - Added logic to show the rate dialog after the app has been launched 3 times.
    - Added the functionality for Myket and Bazaar flavor to open app comment section.
    - Added increase launch count function to manage show rate dialog state.

--- app/src/main/java/com/amirali_apps/tictactoe/MainActivity.kt
+++ app/src/main/java/com/amirali_apps/tictactoe/MainActivity.kt
@@ -8,6 +8,10 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -15,6 +19,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.core.content.edit
 import androidx.core.net.toUri
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavType
@@ -74,7 +81,8 @@ class MainActivity : ComponentActivity() {
             }
             TicTacToeTheme {
                 AppNavHost()
-                var showDialog by remember { mutableStateOf(false) }
+                var showUpdateDialog by remember { mutableStateOf(false) }
+                var showRateDialog by remember { mutableStateOf(increaseLaunchCount(this)) }
                 var response by remember { mutableStateOf<UpdateModel?>(null) }
                 val context = LocalContext.current
                 LaunchedEffect(null) {
@@ -83,7 +91,7 @@ class MainActivity : ComponentActivity() {
                             response = RetrofitClient.retrofitService.checkForUpdate()
                             val latestVersion = response?.latestVersion ?: ""
                             if (latestVersion.isNotEmpty() && latestVersion != BuildConfig.VERSION_NAME) {
-                                showDialog = true
+                                showUpdateDialog = true
                             }
                         } catch (_: Exception) {
                         }
@@ -92,8 +100,8 @@ class MainActivity : ComponentActivity() {
 
 
                 UpdateDialog(
-                    showDialog = showDialog,
-                    onDismiss = { showDialog = false },
+                    showDialog = showUpdateDialog,
+                    onDismiss = { showUpdateDialog = false },
                     onDownloadClick = {
                         val downloadUrl = response?.downloadUrl ?: ""
                         if (downloadUrl.isNotEmpty()) {
@@ -103,8 +111,54 @@ class MainActivity : ComponentActivity() {
                             )
                             context.startActivity(intent)
                         }
-                        showDialog = false
+                        showUpdateDialog = false
                     }
+                )
+                RateDialog(
+                    showDialog = showRateDialog,
+                    onDismiss = {
+                        showRateDialog = false
+                    },
+                    onRateClick = {
+                        when (BuildConfig.FLAVOR) {
+                            "myket" -> {
+                                val intent = context.packageManager
+                                    .getLaunchIntentForPackage("ir.mservices.market")
+
+                                if (intent != null) {
+                                    val url = "myket://comment?id=$packageName"
+                                    val intent = Intent()
+                                    intent.action = Intent.ACTION_VIEW
+                                    intent.data = url.toUri()
+                                    context.startActivity(intent)
+                                } else {
+                                    val intent = Intent(
+                                        Intent.ACTION_VIEW,
+                                        ("https://myket.ir").toUri()
+                                    )
+                                    context.startActivity(intent)
+                                }
+                            }
+
+                            "bazaar" -> {
+                                val intent = context.packageManager
+                                    .getLaunchIntentForPackage("com.farsitel.bazaar")
+                                if (intent != null) {
+                                    val intent = Intent(Intent.ACTION_EDIT)
+                                    intent.data = ("bazaar://details?id=$packageName").toUri()
+                                    intent.setPackage("com.farsitel.bazaar")
+                                    context.startActivity(intent)
+                                } else {
+                                    val intent = Intent(
+                                        Intent.ACTION_VIEW,
+                                        ("https://cafebazaar.ir").toUri()
+                                    )
+                                    context.startActivity(intent)
+                                }
+                            }
+                        }
+                        showRateDialog = false
+                    },
                 )
             }
         }
@@ -134,4 +188,104 @@ fun AppNavHost() {
     }
 }
 
+fun increaseLaunchCount(
+    context: Context,
+): Boolean {
+    val sharedPreferences = context.getSharedPreferences(
+        "app_prefs",
+        Context.MODE_PRIVATE
+    )
+    val launchCount = sharedPreferences.getInt(
+        "launch_count",
+        0
+    )
+    val isIncrementing = sharedPreferences.getBoolean(
+        "is_incrementing",
+        true
+    )
+    if (isIncrementing) {
+        val newLaunchCount = launchCount + 1
+        sharedPreferences.edit() {
+            putInt(
+                "launch_count",
+                newLaunchCount
+            )
+        }
+        if (newLaunchCount == 3) {
+            sharedPreferences.edit {
+                putBoolean(
+                    "is_incrementing",
+                    false
+                )
+            }
+            return true
+        }
+    }
 
+    return false
+}
+
+@Composable
+fun RateDialog(
+    showDialog: Boolean,
+    onDismiss: () -> Unit,
+    onRateClick: () -> Unit
+) {
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = onDismiss,
+            containerColor = MaterialTheme.colorScheme.secondaryContainer,
+            title = {
+                Text(
+                    text = stringResource(R.string.do_you_enjoy_the_game),
+                    style = MaterialTheme.typography.bodyLarge.plus(
+                        TextStyle(
+                            color = MaterialTheme.colorScheme.primary
+                        )
+                    )
+                )
+            },
+            text = {
+                Text(
+                    text = stringResource(
+                        R.string.rate_us_on,
+                        when (BuildConfig.FLAVOR) {
+                            "myket" -> stringResource(R.string.myket)
+                            "bazaar" -> stringResource(R.string.bazaar)
+                            else -> stringResource(R.string.store)
+                        }
+                    ),
+                    style = MaterialTheme.typography.bodyMedium.plus(
+                        TextStyle(
+                            color = MaterialTheme.colorScheme.primary
+                        )
+                    )
+                )
+            },
+            confirmButton = {
+                Button(onClick = onRateClick) {
+                    Text(
+                        stringResource(R.string.rate),
+                        style = MaterialTheme.typography.bodyMedium.plus(
+                            TextStyle(
+                                color = MaterialTheme.colorScheme.secondary
+                            )
+                        )
+                    )
+                }
+            },
+            dismissButton = {
+                Button(onClick = onDismiss) {
+                    Text(
+                        stringResource(R.string.never),
+                        style = MaterialTheme.typography.bodyMedium.plus(
+                            TextStyle(
+                                color = MaterialTheme.colorScheme.secondary
+                            )
+                        )
+                    )
+                }
+            }
+        )
+    }
+}
